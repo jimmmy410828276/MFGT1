43f5f7e8a52d533dfffd4b1b012d2854e7b3119f
jimmmy410828276 <s1082827@gm.pu.edu.tw>
2022-10-03 23:54:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/test/LogicActivity.java
@@ -0,0 +1,132 @@
+package com.example.test;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
+
+public class LogicActivity extends AppCompatActivity {
+
+    //views
+    EditText mEmailEt, mPasswordEt;
+    TextView notHaveAccntTv;
+    Button mLoginBtn;
+
+    //Declare an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
+
+    //progress dialog
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logic);
+
+        //Actionbar and its title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Login");
+        //enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        //In the onCreate() method, initialize the FirebaseAuth instance
+
+
+        //init
+        mEmailEt = findViewById(R.id.emailEt);
+        mPasswordEt = findViewById(R.id.passwordEt);
+        notHaveAccntTv = findViewById(R.id.nothave_accountTv);
+        mLoginBtn = findViewById(R.id.loginBtn);
+
+        //login button click
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //input data
+                String email = mEmailEt.getText().toString();
+                String passw = mPasswordEt.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    //invalid email paatern set error
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                }
+                else {
+                    //valid email pattern
+                    loginUser(email, passw);
+                }
+            }
+        });
+        //not have account textview click
+        notHaveAccntTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LogicActivity.this, RegisterActivity.class));
+            }
+        });
+
+        //init progress dialog
+        pd = new ProgressDialog(this);
+        pd.setMessage("Logging In...");
+    }
+
+    private void loginUser(String email, String passw) {
+        //show progresss dialog
+        pd.show();
+        mAuth.signInWithEmailAndPassword(email, passw)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            //dismiss progress dialog
+                            pd.dismiss();
+                            //Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //user is logged in, so start LoginActivity
+                            startActivity(new Intent(LogicActivity.this, ProfileActivity.class));
+                            finish();
+                        }else{
+                            //dismiss progress dialog
+                            pd.dismiss();
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LogicActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //dismiss progress dialog
+                        pd.dismiss();
+                        //error, get and showerror message
+                        Toast.makeText(LogicActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();//go previous activity
+        return super.onSupportNavigateUp();
+    }
+}
